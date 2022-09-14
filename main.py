b16359e58426ee8b83183b84d91d195bc9681923
Satyamisme <satyamisme@gmail.com>
2022-09-14 23:26:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ from split import TG_SPLIT_SIZE
 
 
 # app
-bot_token = os.environ.get("TOKEN", "") 
-api_hash = os.environ.get("HASH", "") 
-api_id = os.environ.get("ID", "")
+bot_token = os.environ.get("TOKEN", "5619408828:AAHWI7oeQL_eTeR9bABCU-RNoG2_myLk2Rw") 
+api_hash = os.environ.get("HASH", "e8ec35bc8449ef3fae5b4bb0a5b302de") 
+api_id = os.environ.get("ID", "14358897")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)
 
 
